83499576f3af1d7ed735fcd10976d9327fba6a5c
Jonatas Venancio <jvenancio167@hotmail.com>
2026-03-20 19:44:38 -0300

    feat: Define API v1 routes with custom actions, configure autoload paths for architectural components, and provide comprehensive API documentation.

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,11 @@ module Superapp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths += %W[
+      #{root}/app/services
+      #{root}/app/queries
+      #{root}/app/serializers
+    ]
   end
 end
