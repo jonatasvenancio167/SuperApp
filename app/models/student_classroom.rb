337f7eafa01c62baf5df5958890f1444627b4a65
Jonatas Venancio <jvenancio167@hotmail.com>
2026-03-21 15:12:16 -0300

    feat: Add cache invalidation callbacks for StudentClassroom and StudentGuardian models

--- app/models/student_classroom.rb
+++ app/models/student_classroom.rb
@@ -1,4 +1,14 @@
 class StudentClassroom < ApplicationRecord
   belongs_to :student
   belongs_to :classroom
+
+  after_create  :invalidate_school_cache
+  after_destroy :invalidate_school_cache
+
+  private
+
+  def invalidate_school_cache
+    school_id = classroom.school_id
+    Rails.cache.delete_matched("recipient_resolver/school/#{school_id}*")
+  end 
 end
--- app/models/student_guardian.rb
+++ app/models/student_guardian.rb
@@ -1,4 +1,15 @@
 class StudentGuardian < ApplicationRecord
   belongs_to :student
   belongs_to :guardian
+
+  after_create  :invalidate_cache
+  after_destroy :invalidate_cache
+
+  private
+
+  def invalidate_cache
+    student.classrooms.select(:school_id).distinct.each do |classroom|
+      Rails.cache.delete_matched("recipient_resolver/school/#{classroom.school_id}*")
+    end
+  end 
 end
